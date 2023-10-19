bb27e1189f871e873a1063a37c40672fbe08c3c9
Miguel Mosquera <mopri38@gmail.com>
2023-10-19 00:42:38 -0500

    Crear mappers restantes

---
+++ src/main/java/co/edu/tiendaonline/service/mapper/entity/concrete/CorreoElectronicoClienteEntityMapper.java
@@ -0,0 +1,48 @@
+package co.edu.tiendaonline.service.mapper.entity.concrete;
+
+import co.edu.tiendaonline.crosscutting.exception.concrete.ServiceTiendaOnlineException;
+import co.edu.tiendaonline.crosscutting.messages.CatalogoMensajes;
+import co.edu.tiendaonline.crosscutting.messages.enumerator.CodigoMensaje;
+import co.edu.tiendaonline.crosscutting.util.UtilObjeto;
+import co.edu.tiendaonline.data.entity.support.CorreoElectronicoClienteEntity;
+import co.edu.tiendaonline.service.domain.CorreoElectronicoClienteDomain;
+import co.edu.tiendaonline.service.mapper.entity.EntityMapper;
+
+public class CorreoElectronicoClienteEntityMapper implements EntityMapper<CorreoElectronicoClienteEntity, CorreoElectronicoClienteDomain> {
+
+	private static final EntityMapper<CorreoElectronicoClienteEntity, CorreoElectronicoClienteDomain> instancia = new CorreoElectronicoClienteEntityMapper();
+	
+	private CorreoElectronicoClienteEntityMapper() {
+		super();
+	}
+	
+	@Override
+	public final CorreoElectronicoClienteDomain toDomain(final CorreoElectronicoClienteEntity entity) {
+		if(UtilObjeto.esNulo(entity)) {
+			var mensajeUsuario = CatalogoMensajes.obtenerContenidoMensaje(CodigoMensaje.M0000000004);
+			var mensajeTecnico = CatalogoMensajes.obtenerContenidoMensaje(CodigoMensaje.M0000000080);
+			throw ServiceTiendaOnlineException.crear(mensajeUsuario, mensajeTecnico);
+		}
+		 
+		return CorreoElectronicoClienteDomain.crear(entity.getCorreoElectronico(), entity.isCorreoElectronicoConfirmado());
+	}
+
+	@Override
+	public final CorreoElectronicoClienteEntity toEntity(final CorreoElectronicoClienteDomain domain) {
+		if(UtilObjeto.esNulo(domain)) {
+			var mensajeUsuario = CatalogoMensajes.obtenerContenidoMensaje(CodigoMensaje.M0000000004);
+			var mensajeTecnico = CatalogoMensajes.obtenerContenidoMensaje(CodigoMensaje.M0000000081);
+			throw ServiceTiendaOnlineException.crear(mensajeUsuario, mensajeTecnico);
+		}
+		
+		return CorreoElectronicoClienteEntity.crear(domain.getCorreoElectronico(), domain.isCorreoElectronicoConfirmado());
+	}
+
+	public static final CorreoElectronicoClienteDomain convertToDomain(final CorreoElectronicoClienteEntity entity) {		 
+		return instancia.toDomain(entity);
+	}
+	
+	public static final CorreoElectronicoClienteEntity convertToEntity(final CorreoElectronicoClienteDomain domain) {
+		return instancia.toEntity(domain);
+	}
+}
---
+++ src/main/java/co/edu/tiendaonline/service/mapper/entity/concrete/NombreCompletoClienteEntityMapper.java
@@ -0,0 +1,48 @@
+package co.edu.tiendaonline.service.mapper.entity.concrete;
+
+import co.edu.tiendaonline.crosscutting.exception.concrete.ServiceTiendaOnlineException;
+import co.edu.tiendaonline.crosscutting.messages.CatalogoMensajes;
+import co.edu.tiendaonline.crosscutting.messages.enumerator.CodigoMensaje;
+import co.edu.tiendaonline.crosscutting.util.UtilObjeto;
+import co.edu.tiendaonline.data.entity.support.NombreCompletoClienteEntity;
+import co.edu.tiendaonline.service.domain.NombreCompletoClienteDomain;
+import co.edu.tiendaonline.service.mapper.entity.EntityMapper;
+
+public class NombreCompletoClienteEntityMapper implements EntityMapper<NombreCompletoClienteEntity, NombreCompletoClienteDomain> {
+
+	private static final EntityMapper<NombreCompletoClienteEntity, NombreCompletoClienteDomain> instancia = new NombreCompletoClienteEntityMapper();
+	
+	private NombreCompletoClienteEntityMapper() {
+		super();
+	}
+	
+	@Override
+	public final NombreCompletoClienteDomain toDomain(final NombreCompletoClienteEntity entity) {
+		if(UtilObjeto.esNulo(entity)) {
+			var mensajeUsuario = CatalogoMensajes.obtenerContenidoMensaje(CodigoMensaje.M0000000004);
+			var mensajeTecnico = CatalogoMensajes.obtenerContenidoMensaje(CodigoMensaje.M0000000082);
+			throw ServiceTiendaOnlineException.crear(mensajeUsuario, mensajeTecnico);
+		}
+		 
+		return NombreCompletoClienteDomain.crear(entity.getPrimerNombre(), entity.getSegundoNombre(), entity.getPrimerApellido(), entity.getSegundoApellido());
+	}
+
+	@Override
+	public final NombreCompletoClienteEntity toEntity(final NombreCompletoClienteDomain domain) {
+		if(UtilObjeto.esNulo(domain)) {
+			var mensajeUsuario = CatalogoMensajes.obtenerContenidoMensaje(CodigoMensaje.M0000000004);
+			var mensajeTecnico = CatalogoMensajes.obtenerContenidoMensaje(CodigoMensaje.M0000000083);
+			throw ServiceTiendaOnlineException.crear(mensajeUsuario, mensajeTecnico);
+		}
+		
+		return NombreCompletoClienteEntity.crear(domain.getPrimerNombre(), domain.getSegundoNombre(), domain.getPrimerApellido(), domain.getSegundoApellido());
+	}
+
+	public static final NombreCompletoClienteDomain convertToDomain(final NombreCompletoClienteEntity entity) {		 
+		return instancia.toDomain(entity);
+	}
+	
+	public static final NombreCompletoClienteEntity convertToEntity(final NombreCompletoClienteDomain domain) {
+		return instancia.toEntity(domain);
+	}
+}
